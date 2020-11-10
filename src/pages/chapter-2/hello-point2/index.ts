fadee7661c61f4b82d4f8ad524640ffdff06f8d1
Aleksandr Sidorenko <mcmerph@gmail.com>
2020-11-10 22:49:56 +0300

    Add hello-point2

---
+++ src/pages/chapter-2/hello-point2/index.ts
@@ -0,0 +1,38 @@
+import 'normalize.css';
+
+import initShaders from '../../../init-shaders';
+import VSHADER_SOURCE from './point.vert';
+import FSHADER_SOURCE from './point.frag';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('webgl') as HTMLCanvasElement;
+  if (!canvas) {
+    throw new Error('Failed to retrieve the <canvas> element');
+  }
+
+  const gl = canvas.getContext('webgl');
+  if (!gl) {
+    throw new Error('Failed to get the rendering context for WebGL');
+  }
+
+  const glProgram = initShaders({
+    gl,
+    vertexShaderSource: VSHADER_SOURCE,
+    fragmentShaderSource: FSHADER_SOURCE,
+  });
+
+  const a_Position = gl.getAttribLocation(glProgram, 'a_Position');
+  if (a_Position < 0) {
+    throw new Error('Failed to get the storage location of a_Position');
+  }
+  const a_PointSize = gl.getAttribLocation(glProgram, 'a_PointSize');
+  if (a_PointSize < 0) {
+    throw new Error('Failed to get the storage location of a_PointSize');
+  }
+
+  gl.vertexAttrib3f(a_Position, 0.5, 0.5, 0.0);
+  gl.vertexAttrib1f(a_PointSize, 5.0);
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, 1);
+});
