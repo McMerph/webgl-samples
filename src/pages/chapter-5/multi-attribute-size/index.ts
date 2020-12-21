bd439a7acdfceb619c6d16f49e8a04e474f191b4
Aleksandr Sidorenko <mcmerph@gmail.com>
2020-12-21 22:23:40 +0300

    Add multiattribute size

---
+++ src/pages/chapter-5/multi-attribute-size/index.ts
@@ -0,0 +1,63 @@
+import 'normalize.css';
+
+import initShaders from '../../../init-shaders';
+import VSHADER_SOURCE from './point.vert';
+import FSHADER_SOURCE from './point.frag';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('webgl') as HTMLCanvasElement;
+  if (!canvas) {
+    throw new Error('Failed to retrieve the <canvas> element');
+  }
+
+  const gl = canvas.getContext('webgl');
+  if (!gl) {
+    throw new Error('Failed to get the rendering context for WebGL');
+  }
+
+  const glProgram = initShaders({
+    gl,
+    vertexShaderSource: VSHADER_SOURCE,
+    fragmentShaderSource: FSHADER_SOURCE,
+  });
+
+  // prettier-ignore
+  const vertices = new Float32Array([
+    0.0, 0.5,
+    -0.5, -0.5,
+    0.5, -0.5
+  ]);
+  const sizes = new Float32Array([
+    10.0, 20.0, 30.0
+  ]);
+
+  const vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+    throw new Error('Failed to create the vertex buffer');
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+  const a_Position = gl.getAttribLocation(glProgram, 'a_Position');
+  if (a_Position < 0) {
+    throw new Error('Failed to get the storage location of a_Position');
+  }
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_Position);
+
+  const sizeBuffer = gl.createBuffer();
+  if (!sizeBuffer) {
+    throw new Error('Failed to create the size buffer');
+  }
+  gl.bindBuffer(gl.ARRAY_BUFFER, sizeBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, sizes, gl.STATIC_DRAW);
+  const a_PointSize = gl.getAttribLocation(glProgram, 'a_PointSize');
+  if (a_PointSize < 0) {
+    throw new Error('Failed to get the storage location of a_PointSize');
+  }
+  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(a_PointSize);
+
+  gl.clearColor(0, 0, 0, 1);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.drawArrays(gl.POINTS, 0, 3);
+});
